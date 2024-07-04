978a1a085daf373a157cdc7c10569f3546a6acf4
Thalles <aguiarsofya856@gmail.com>
2024-07-04 18:18:06 -0300

    Exercicio 03

---
+++ exercicios-backend-variaveis/exercicio03.js
@@ -0,0 +1,20 @@
+##Quantidade de Cerca 
+
+##Problema
+Você foi contratado para desenvolver um programa que calcule a quantidade de terreno necessária para cercar um terreno.
+Os terrenos possuem forma retangular. A quantidade de arranjos necessários é dada por: quantidade = comprimento * 2 + largura * 2
+Você deve RETORNAR a quantidade de arranjo necessária para cercar o terreno.
+
+
+##Código
+
+function solucao(comprimento, largura) {
+    let comprimento = 20
+    let largura = 15
+     const quantidade = (2 * comprimento) + (2 * largura)
+  
+    console.log(quantidade)
+    return quantidade
+  }
+  
+  module.exports = solucao;
