129bc0884e890554603c73f1459ebb9cf4c91f7a
Thalles <aguiarsofya856@gmail.com>
2024-07-04 21:56:12 -0300

    Exercicio 01

---
+++ exercicios-backend-ts/exercicio01.ts
@@ -0,0 +1,10 @@
+##Atualização da pontuação
+
+##Problema
+Você foi contratado pela Ubisoft, uma das maiores empresas de desenvolvimento de jogos eletrônicos do mundo, para desenvolver o Cubos Game, um novo jogo para Playstation 5.
+Foi atribuida a você a tarefa de desenvolver a função responsável por atualizar a pontuação do personagem do jogo.
+Você deverá RETORNAR a nova pontuação do jogador.
+
+
+##Código
+
