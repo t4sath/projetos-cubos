d9b5de62b8e740f01f2380c361618f921d77cef2
Thalles <aguiarsofya856@gmail.com>
2024-07-04 22:00:30 -0300

    Exercicio 02

---
+++ exercicios-backend-ts/exercicio02.ts
@@ -0,0 +1,10 @@
+##Cálculo da idade
+
+##Problema
+O jogo Cubos Game, para o qual foi contratado para fazer, só poderá ser jogado por maiores de 16 anos. Sendo assim, será necessário criar uma função que calcule a idade do jogador com base no seu ano de nascimento.
+Você deve RETORNAR a idade do jogador.
+OBS: Não serão levados em consideração o mês e o dia no cálculo.
+
+
+##Código
+
