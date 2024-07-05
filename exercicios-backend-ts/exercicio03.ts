a925f0f027291d57ba904977d5f695c54a20e202
Thalles <aguiarsofya856@gmail.com>
2024-07-04 23:40:34 -0300

    Exercicio 03

---
+++ exercicios-backend-ts/exercicio03.ts
@@ -0,0 +1,11 @@
+##Dados do personagem
+
+##Problema
+O jogador deverá conseguir mostrar os dados do seu personagem no jogo Cubos Game.
+Você deve RETORNAR os dados do personagem no seguinte formato:
+OLÁ, |FULANO|. VOCE É UM |CLASSE| COM PONTUAÇÃO ATUAL DE |PONTUACAO_ATUAL|
+Você deve substituir |FULANO|, |CLASSE| e |PONTUACAO_ATUAL| por, respectivamente, o nome, a classe e a pontuação do jogador.
+
+
+##Código
+
