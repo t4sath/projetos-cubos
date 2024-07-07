236ee0e948c4f00ff1484f01587766f134f14f2e
Thalles <aguiarsofya856@gmail.com>
2024-07-06 23:28:06 -0300

    Exercicio 05

---
+++ exercicios-backend-ts/exercicio06.ts
@@ -0,0 +1,11 @@
+##Quantidade de carne
+
+##Problema
+Você está desenvolvendo um programa que calcula a quantidade de carne que deverá ser comprada para o churrasco anual da sua família.
+A quantidade de carne é calculada da seguinte forma:
+100 gramas para cada criança na festa
+200 gramas para cada pré-adolescente
+300 gramas para cada adulto
+Você deve RETORNAR a quantidade de carne estimada, em gramas.
+
+##Código
---
+++ exercicios-backend-ts/exercicio07.ts
@@ -0,0 +1,8 @@
+##Cálculo do IMC
+
+##Problema
+Você, como desenvolvedor de software de um grupo farmacéutico, deve implementar uma funcionalidade nova no programa da empresa: o cálculo do Índice de Massa Corporal.
+O IMC é cálculado como: peso / altura * altura
+Você deve RETORNAR o IMC.
+
+##Código
