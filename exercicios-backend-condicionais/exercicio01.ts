d3112a4bc46cd6e4c6d195235f7b3b4c759f7d97
Thalles <aguiarsofya856@gmail.com>
2024-07-22 22:50:00 -0300

    Exercicio 01

---
+++ exercicios-backend-condicionais/exercicio01.ts
@@ -0,0 +1,24 @@
+# Presa ou predador
+
+## Problema
+Em uma reportagem de 2016, a revista Veja cita as principais características que indicam se um animal é presa ou predador. Uma delas, segundo a revista, é que as presas costumam ter os olhos nos lados da cabeça, o que oferece um amplo ângulo de visão.
+Martin Banks, professor de óptica da Universidade de Berkeley, diz o seguinte:
+“A primeira necessidade desses animais (presas) é detectar predadores. Por isso, desenvolveram uma visão panorâmica com poucos pontos cegos”.
+Você foi contratado pelo grupo de pesquisadores que desenvolveram essa pesquisa para criar um programa que diga se um animal é presa ou predador.
+
+Seu programa deve RETORNAR:
+- `PRESA`: se o animal tiver olhos nas laterais da cabeça
+- `PREDADOR`: se não tiver olhos nas laterais da cabeça
+
+
+##Código:
+function solucao(olhosNasLaterais: boolean): string {
+  if (olhosNasLaterais = true) {
+    return 'Presa'
+  } else {
+    return 'Predador'
+  }
+
+console.log (olhosNasLaterais)
+return olhosNasLaterais
+}
